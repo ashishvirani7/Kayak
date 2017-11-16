7b215e2b04275b89ede0ea3279498f8246a53e88
Ashish Virani <ashishvirani10@gmail.com>
2017-11-16 11:42:48 -0800

    first commit

---
+++ src/components/UnAuthorizedRoute.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {Route,Redirect} from 'react-router-dom';
+import * as API from '../api/api';
+import {changeUserState} from '../actions/action.js';
+
+class UnAuthorizedRoute extends Component {
+
+  componentWillMount(){
+    this.props.changeUserState(true,false);
+    API.checkSession()
+    .then((data)=>{
+      if(data.status==='201'){
+        this.props.changeUserState(false,true);
+      }
+      else{
+        this.props.changeUserState(false,false);
+      }
+    });
+  }
+  
+    render() {
+      const { component: Component, ...rest } = this.props
+      return (
+        <Route {...rest} render={props => {
+          if (this.props.userstate.pending===undefined || this.props.userstate.pending===true) return <div>...Loading...</div>
+          return !(this.props.userstate.logged)
+            ? <Component {...this.props} />
+            : <Redirect to="/" />
+        }} />
+      )
+    }
+  }
+  
+  const mapStateToProps= state =>{
+    return{
+        afterAuth:state.afterAuth,
+        userstate: state.userstate
+    };
+  }
+
+  const mapDispatchToProps = dispatch =>{
+    return{
+        changeUserState: (p,l) =>dispatch(changeUserState(p,l))
+    }
+}
+  
+  export default connect(mapStateToProps,mapDispatchToProps)(UnAuthorizedRoute)
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+import {Provider} from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import {compose, applyMiddleware, createStore} from 'redux';
+import {persistStore, autoRehydrate} from 'redux-persist';
+import reduxReset from 'redux-reset';
+import allReducers from './reducers';
+
+import {MuiThemeProvider,createMuiTheme} from 'material-ui/styles';
+
+const enHanceCreateStore = compose(
+    reduxReset(),
+    autoRehydrate(),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && 
+    window.__REDUX_DEVTOOLS_EXTENSION__(),
+  )(createStore)
+const store = enHanceCreateStore(allReducers)
+
+//persistStore(store);
+const theme = createMuiTheme();
+
+persistStore(store, {}, () => {
+    ReactDOM.render(    
+        <Provider store = {store}>
+            <MuiThemeProvider theme={theme}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </MuiThemeProvider>
+        </Provider>
+    , 
+    document.getElementById('root')
+    );
+})
+registerServiceWorker();
+
