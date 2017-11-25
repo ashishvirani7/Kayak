a576d74a27da2961a28c9d8844aa916be81b2306
Niral Koradia <koradiya.niral@gmail.com>
2017-11-25 12:58:25 -0800

    Mongoose in Signup
    Usage of Mongoose in Signup

--- kafkabackend/services/signup.js
+++ kafkabackend/services/signup.js
@@ -1,15 +1,82 @@
-var mongo = require("./mongo");
-var mongoURL = "mongodb://localhost:27017/kayak";
-var autoIncrement = require("mongodb-autoincrement");
+// var mongo = require("./mongo");
+// var mongoURL = "mongodb://localhost:27017/kayak";
 var bcrypt = require('bcrypt');
 var CryptoJS = require("crypto-js");
+
+var mongoose = require('mongoose');
+mongoose.connect('localhost:27017/kayak');
+var Users = require('../models/Users');
 
 const saltRounds = 10;
 
 function handle_request(msg, callback){
 
     var res = {};
+    var message = "";
+    var email = msg.email
+    var password = msg.password;
+    var bytes  = CryptoJS.AES.decrypt(password.toString(), "kayak");
+    password = bytes.toString(CryptoJS.enc.Utf8);
+
     console.log("In handle request:"+ JSON.stringify(msg));
+
+    Users.count({email:email}, function (err, count) {
+        if(err)
+        {
+            message ="Some Error Happened while checking user ID";
+            console.log(message);
+            res.code="500";
+            res.data=message;
+            callback(null,res);
+        }
+        else {
+            if (count > 0) {
+                //User exists
+                message="User :"+email+" Exists";
+                console.log(message);
+                res.code="202";
+                res.data=message;
+                callback(null,res);
+            }
+            else {
+                message="Insert User";
+                console.log(message);
+
+                bcrypt.hash(password, saltRounds, function(err, hash) {
+                    if(!err) {
+                        var userObject = {
+                            email: email,
+                            password: hash
+                        };
+                        var user = new Users(userObject);
+                        user.save(function (err, product, numAffected) {
+                            if (err) {
+                                console.log("Some Error Happened while Inserting user");
+                                res.code = "500";
+                                res.data = message;
+                                callback(null, res);
+                            }
+                            else {
+                                message = numAffected + " rows added into Cart\n" + product;
+                                console.log(message);
+                                res.code = "201";
+                                res.data = "Signup Successful";
+                                callback(null, res);
+                            }
+                        });
+                    }
+                    else
+                        {
+                            message = "Some Error Happened while Hashing";
+                            console.log(message);
+                            res.code = "500";
+                            res.data = message;
+                            callback(null, res);
+                    }
+                });
+            }
+        }
+    });
 
     // Without Connection Pooling
     try {
