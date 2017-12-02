755f2569fa5e81f259a76d05b4a9d3a1f9222f54
monank savliya <monank.savaliya@sjsu.edu>
2017-12-02 04:19:33 -0800

    payment method done

--- kayakfrontend/src/components/UserBooking.js
+++ kayakfrontend/src/components/UserBooking.js
@@ -16,6 +16,8 @@ import {withRouter} from 'react-router-dom';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import Paper from 'material-ui/Paper';
+import AA from '../images/DL.png';
+import PaymentPage from './PaymentPage';
 
 
 class UserBooking extends Component{
@@ -67,37 +69,114 @@ class UserBooking extends Component{
     }
 
     showBookingDetails = () => {
+
+        var booking = this.props.userData.booking;
         return(
-            <Paper style={paperstyle} zDepth={3}>
-                {this.userData.booking.bookingType==="Hotel" && 
-                    <div className="row">
-                        <div className="col-md-6">
-
+            <div className="row">
+                <div className="col-md-7">
+                <Paper style={flightstyle} zDepth={3}>
+                    {booking.bookingType==="Flight" && 
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div className="row">
+                                    <div className="col-md-2">
+                                        <div className="row" style={{marginTop:'30px',marginLeft:'10px'}}>
+                                            <img src={AA} style={{width:'32px'}}/>
+                                        </div>
+                                        <div className="row" style={{marginTop:'20px'}}>
+                                            {booking.flight.flight_operator_name}
+                                        </div>
+                                    </div>
+                                    <div className="col-md-2">
+                                        <div className="row" style={{marginTop:'40px'}}>
+                                            {booking.flight.departure_date.slice(11,16)}
+                                        </div>
+                                        <div className="row" style={{marginTop:'10px'}}>
+                                            {booking.flight.departure_date.slice(0,10)}
+                                        </div>
+                                        <div className="row">
+                                            {booking.flight.origin}
+                                        </div>
+                                    </div>
+                                    <div className="col-md-3">
+                                    <div className="row" style={{marginTop:'60px'}}>
+                                            <span style={{width:'100%',height:'2px',display:'inline-block',background:'#717585',position:'relative',margin:'3px 0'}} />
+                                        </div>
+                                        <div className="row" style={{textAlign:'center'}}>
+                                            {'nonstop'}
+                                        </div>  
+                                    </div>
+                                    <div className="col-md-2 col-md-offset-1">
+                                        <div className="row" style={{marginTop:'40px'}}>
+                                            {booking.flight.arrival_date.slice(11,16)}  
+                                        </div>
+                                        <div className="row" style={{marginTop:'10px'}}>
+                                            {booking.flight.arrival_date.slice(0,10)}  
+                                        </div>
+                                        <div className="row" >
+                                            {booking.flight.destination}
+                                        </div>
+                                    </div>
+                                    <div className="col-md-2">
+                                        <div className="row" style={{marginTop:'50px'}}>
+                                            {'21h 50m'}
+                                            {booking.flight.duration}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="row">
+                                    
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                }
-                {this.userData.booking.bookingType==="Flight" && 
-                    <div className="row">
-                        <div className="col-md-6">
-
+                    }
+                    {booking.bookingType==="Hotel" && 
+                        <div className="row">
+                            <div className="col-md-6">
+                                hello
+                            </div>
                         </div>
-                    </div>
-                }
-                {this.userData.booking.bookingType==="Car" && 
-                    <div className="row">
-                        <div className="col-md-6">
+                    }
+                    {booking.bookingType==="Car" && 
+                        <div className="row">
+                            <div className="col-md-6">
 
+                            </div>
                         </div>
+                    }
+                </Paper>
+                </div>
+                <div className="col-md-4">
+                <Paper style={{...flightstyle,width:'250px',marginLeft:'-100px'}} zDepth={3}>
+                    <div className="row">
+                        Name: {this.props.userData.data.first_name}
+                        <br/>
+                        Class: {booking.search.class}
+                        <br/>
+                        No Of Travellers: {booking.search.no_of_traveler}
+                        <br/>
+                        price: 
+                        {(booking.flight.classes[0].class_type === booking.search.class) && booking.flight.classes[0].class_price}
+                        {(booking.flight.classes[1].class_type === booking.search.class) && booking.flight.classes[1].class_price}
+                        {(booking.flight.classes[2].class_type === booking.search.class) && booking.flight.classes[2].class_price}
+                        <br/>
+                        Total: 
+                        {(booking.flight.classes[0].class_type === booking.search.class) && booking.flight.classes[0].class_price*booking.search.no_of_traveler}
+                        {(booking.flight.classes[1].class_type === booking.search.class) && booking.flight.classes[1].class_price*booking.search.no_of_traveler}
+                        {(booking.flight.classes[2].class_type === booking.search.class) && booking.flight.classes[2].class_price*booking.search.no_of_traveler}
                     </div>
-                }
-            </Paper>
+                </Paper>
+                </div>
+            </div>
         )
     }
 
     showPaymentDetails = () => {
         return(
-            <Paper style={paperstyle} zDepth={3}>
-
+            <Paper style={paymentstyle} zDepth={3}>
+                <PaymentPage/>
             </Paper>
         )
     }
@@ -123,7 +202,7 @@ class UserBooking extends Component{
                 <div className="row" style={{margin:'20px -30px 0px 50px',height:'50px',fontSize:'30px',fontWeight:'500px'}}>
                     {this.props.userData.booking.bookingType} Booking
                 </div>
-                <div className="row" style={{marginRight:'-30px',height:'110vh'}}>
+                <div className="row" style={{marginRight:'-30px',minHeight:'90vh'}}>
                 <div style={{ margin: '20px 50px 50px 50px'}}>
                     <Stepper activeStep={stepIndex} orientation="vertical">
                     <Step>
@@ -167,8 +246,24 @@ const paperstyle = {
     height: 200,
     width: 700,
     margin: 20,
-    textAlign: 'center',
     display: 'inline-block',
+    padding:'15px'
+  };
+
+const flightstyle = {
+    height: 200,
+    width: 600,
+    margin: 20,
+    display: 'inline-block',
+    padding:'15px 20px 15px 50px'
+  };
+
+const paymentstyle = {
+    height: 750,
+    width: 600,
+    margin: 20,
+    display: 'inline-block',
+    padding:'15px 20px 15px 50px'
   };
 
 function mapStateToProps(state){
