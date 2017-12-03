40be411cb2b9eec0d86f19caa20e3d47d7e6e616
sonijigar <jigarsoni1111@gmail.com>
2017-12-02 17:46:49 -0800

    get revenue for hotels/flights/cars

--- kafkabackend/services/getRevenue.js
+++ kafkabackend/services/getRevenue.js
@@ -2,45 +2,114 @@ var mongoose = require('mongoose');
 mongoose.connect('54.183.101.173:27017/kayak');
 var Bill = require('../models/Bill');
 
-
-function handle_hotels(msg, callback) {
+function handle_hotels(msg, callback){
     var res = {};
     var message = "";
-    var nextMonth;
-    var nextYear;
-
+    var nextMonth = "02";
+    var nextYear = msg.year;
+    var thisMonth = "01";
+    var arr = [];
     console.log("In handle request:" + JSON.stringify(msg));
-    if(parseInt(msg.month) == 12){
-        nextMonth = 1;
-        nextYear = parseInt(msg.year)+1;
-        nextMonth = 0+""+nextMonth;
-    }
-    else{
-        nextMonth = parseInt(msg.month)+1;
-        if(nextMonth<10){
-            nextMonth = ""+0+nextMonth;
+    Bill.aggregate(
+        [
+            {
+               // bill_date :{$month:}
+                $match:{"hotel.hotel_name":msg.hotelName, "bill_type":"Hotel"}
+            },
+            {
+                $group:{_id:{month:{$month:"$bill_date"}}, total:{$sum:"$hotel.amount"}}
+            }
+        ], function(err, hotels){
+            if(err){
+                        message="error"
+                        res.code = "202"
+                        res.data = message;
+                        callback(null, res)
+                    }
+                    else{
+                        res.code = "201"
+                        res.data = hotels;
+                        console.log(hotels);
+                        callback(null, res);
+                    }
         }
-        nextYear = parseInt(msg.year);
-    }
-    console.log(nextYear+"-"+nextMonth+"-"+"01T00:00:00.000Z")
-    Bill.aggregate([
-        {$match:{"hotel.hotel_name":msg.hotelName, "bill_date":{$gte:new Date(nextYear+"-"+nextYear+"-"+"01T00:00:00.000Z"),$lte:new Date(""+nextYear+"-"+nextMonth+"-"+"01T00:00:00.000Z")}}},
-        {
-        $group:{_id:msg.hotelName, total:{$sum:"$hotel.amount"}}
-    }], function(err, hotels){
-        if(err){
-            message="error"
-            res.code = "202"
-            res.data = message;
-            callback(null, res)
+    )
+
+
+}
+
+function handle_flights(msg, callback){
+    var res = {};
+    var message = "";
+    var nextMonth = "02";
+    var nextYear = msg.year;
+    var thisMonth = "01";
+    var arr = [];
+    console.log("In handle request:" + JSON.stringify(msg));
+    Bill.aggregate(
+        [
+            {
+                // bill_date :{$month:}
+                $match:{"flight.flight_operator_name":msg.hotelName, "bill_type":"Flight"}
+            },
+            {
+                $group:{_id:{month:{$month:"$bill_date"}}, total:{$sum:"$flight.amount"}}
+            }
+        ], function(err, hotels){
+            if(err){
+                message="error"
+                res.code = "202"
+                res.data = message;
+                callback(null, res)
+            }
+            else{
+                res.code = "201"
+                res.data = hotels;
+                console.log(hotels);
+                callback(null, res);
+            }
         }
-        else{
-            res.code = "201"
-            res.data = hotels;
-            console.log(hotels);
-            callback(null, res);
+    )
+
+
+}
+
+function handle_cars(msg, callback){
+    var res = {};
+    var message = "";
+    var nextMonth = "02";
+    var nextYear = msg.year;
+    var thisMonth = "01";
+    var arr = [];
+    console.log("In handle request:" + JSON.stringify(msg));
+    Bill.aggregate(
+        [
+            {
+                // bill_date :{$month:}
+                $match:{"car.car_type":msg.hotelName, "bill_type":"Car"}
+            },
+            {
+                $group:{_id:{month:{$month:"$bill_date"}}, total:{$sum:"$car.amount"}}
+            }
+        ], function(err, hotels){
+            if(err){
+                message="error"
+                res.code = "202"
+                res.data = message;
+                callback(null, res)
+            }
+            else{
+                res.code = "201"
+                res.data = hotels;
+                console.log(hotels);
+                callback(null, res);
+            }
         }
-    })
+    )
+
+
 }
 
 exports.handle_hotels = handle_hotels;
+exports.handle_flights = handle_flights;
+exports.handle_cars = handle_cars;
--- kayakbackend/routes/getRevenue.js
+++ kayakbackend/routes/getRevenue.js
@@ -6,10 +6,9 @@ var topic_name = "get_revenue_topic";
 
 router.post('/hotels', (req, res, next)=>{
     var hotelName = req.body.hotelName;
-    var month = req.body.month;
     var year = req.body.year;
     var key = "hotels";
-    kafka.make_request(topic_name, {key, hotelName, month, year}, function(err, results){
+    kafka.make_request(topic_name, {key, hotelName, year}, function(err, results){
         if(err){
             done(err,{});
         }
@@ -29,21 +28,42 @@ router.post('/hotels', (req, res, next)=>{
 
 router.post('/flights', (req, res, next)=>{
     var hotelName = req.body.operatorName;
-    var month = req.body.month;
     var year = req.body.year;
     var key = "flights";
-    kafka.make_request(topic_name, {key, hotelName, month, year}, function(err, results){
+    kafka.make_request(topic_name, {key, hotelName, year}, function(err, results){
         if(err){
             done(err,{});
         }
         else
         {
             if(results.code == 201){
-                console.log("Hotel Revenue found")
+                console.log("Flight Revenue found")
                 return res.status(201).send(results);
             }
             else if(results.code == 202){
-                console.log("Hotel Revenue not found")
+                console.log("Flight Revenue not found")
+                return res.status(202).send(results);
+            }
+        }
+    })
+});
+
+router.post('/cars', (req, res, next)=>{
+    var hotelName = req.body.carType;
+    var year = req.body.year;
+    var key = "cars";
+    kafka.make_request(topic_name, {key, hotelName, year}, function(err, results){
+        if(err){
+            done(err,{});
+        }
+        else
+        {
+            if(results.code == 201){
+                console.log("Car Revenue found")
+                return res.status(201).send(results);
+            }
+            else if(results.code == 202){
+                console.log("Car Revenue not found")
                 return res.status(202).send(results);
             }
         }
